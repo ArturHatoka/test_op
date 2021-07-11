3c6684b353773c9e9b5fead0d9a2465658956728
khasanshinae <khasanshinae@oooinex.ru>
2021-07-11 13:21:59 +0300

    123

--- assets/dev/js/scripts.js
+++ assets/dev/js/scripts.js
@@ -1,1 +1,9 @@
-console.log('asasa')
+import Swiper from 'swiper/bundle';
+
+new Swiper('.swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- assets/dev/preAssets/js/scripts.js
+++ assets/dev/preAssets/js/scripts.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log('asasa');
+var _bundle = _interopRequireDefault(require("swiper/bundle"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+new _bundle["default"]('.swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
