a4c85b8363ef68dc5b172e01f46235750b281d5c
khasanshinae <khasanshinae@oooinex.ru>
2021-07-11 18:20:36 +0300

    init123

--- assets/dev/js/scripts.js
+++ assets/dev/js/scripts.js
@@ -2,8 +2,21 @@ import Swiper from 'swiper/bundle';
 
 new Swiper('.swiper-container', {
   loop: true,
+  spaceBetween: 250,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+const show_more = document.querySelectorAll('button[data-action="show_more"]')
+show_more.forEach((show)=>{
+  show.addEventListener('click',() => {
+    removeClosed(show)
+  })
+})
+
+let removeClosed = (elem) =>{
+  elem.parentNode.querySelector('.closed').classList.remove('closed')
+  elem.classList.add('d-none')
+}
--- assets/dev/preAssets/js/scripts.js
+++ assets/dev/preAssets/js/scripts.js
@@ -6,8 +6,20 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 new _bundle["default"]('.swiper-container', {
   loop: true,
+  spaceBetween: 250,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   }
 });
+var show_more = document.querySelectorAll('button[data-action="show_more"]');
+show_more.forEach(function (show) {
+  show.addEventListener('click', function () {
+    removeClosed(show);
+  });
+});
+
+var removeClosed = function removeClosed(elem) {
+  elem.parentNode.querySelector('.closed').classList.remove('closed');
+  elem.classList.add('d-none');
+};
